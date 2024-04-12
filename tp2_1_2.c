6761eedb3f60a10e8fc085d165124a445a2a4501
Gonzalo Martinez <mgonzalo192@gmail.com>
2024-04-12 11:50:17 -0300

    corregido el punto 1_2 y terminado 1_3

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -7,21 +7,29 @@ int main (int argc, char* argv[])
 {
     // declaraciones
     int i;
-    double vt[N];       // primero declaras el arreglo
-    double *pvt;        // despues declaras el puntero
+    double vt[N], *pvt;       // primero declaras el arreglo, despues declaras el puntero
 
     // asignaciones
     pvt = vt;           // recien ahora podes asignar la direccion
     // pvt = &vt[0];    // equivale a la linea de arriba
 
+    // pero pide usar notacion indexada osea *(pvt + 1) no pvt++
     for(int i = 0; i < N; i++)
     {
-        *pvt = 1 + rand() % 100;    // genera un numero y lo asigna a la direccion de memoria del elemento
+        *pvt = 1 + rand() % 100;        // genera un numero y lo asigna a la direccion de memoria del elemento
 
-        printf("%f\n", *pvt);       // muestra lo que está en esa direccion
-
-        pvt++;                      // mueve el puntero a la direccion siguiente
+        printf("%f\n", *(pvt++));       // muestra lo que está en esa direccion
+        /* De la teoria:
+            *(pvt++) usa lo referenciado e incrementa la direccion
+            osea primero usas el valor en vt[i] y despues moves
+            la referencia a vt[i + 1]
+        */
     }
-    
+        /* este era un error grave:
+            usar *(pvt + 1) en printf para despues usar pvt++
+            para moverte en el arreglo parece desfasar el indice que queres usar
+            haciendo eso si bien el vector si se rellenaba con valores nuevos
+            cuando los queres mostrar solo terminas mostrando ceros
+        */
     return 0;
 }
--- tp2_3.c
+++ tp2_3.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+#define N 5    // numero de filas
+#define M 7    // numero de columans
+
+int main(int argc, char *argv[])
 {
-    // constantes
-    #define M 7     // filas
-    #define N 5     // columnas
+// podrias hacer esta matriz de 2 formas distintas:
+//    - con forma de matriz:
+//        int matriz[N][M], *pmatriz;
+//        pmatriz = &matriz[0][0];
 
-    // declaraciones
-    int i,j;
-    int numero;
-    int mt[M][N];
-    int *pmt;
+//  - o con un vector en lugar de una matriz:
+    int matriz[35], *pmatriz;
+    pmatriz = matriz;
 
-    for (int i = 0; i < M; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            mt[i][j] = 1 + rand() % 100;   // hasta acá asignaste la matriz
-        }
-    }
-    
+    printf("\n");
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
         {
-            if (*(mt + ((i * M) + j)) < 10)
+            *(pmatriz + i * M + j) = 1 + rand() % 100;
+
+            if (*(pmatriz + i * M + j) < 10)
             {
-                printf("0%d " , *(mt + ((i * M) + j)));
+                printf("0%i ", *(pmatriz + i * M + j));
             }
             else
             {
-                printf("%d " , *(mt + ((i * M) + j)));
+                printf("%i ", *(pmatriz + i * M + j));
             }
         }
         printf("\n");
     }
+    printf("\n");
 
     return 0;
 }
